2450f58ddef89d306d66659a58f70b49604e7c98
usuario <usuario@mail.hostingjoseliki.com>
2014-02-06 20:36:57 +0100

    prueba aplicacion meteosix

--- meteosix.py
+++ meteosix.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
 import urllib2
-import json
-request = urllib2.Request('http://servizos.meteogalicia.es/apiv2/getTidesInfo?coord=-8.350573861318628,43.3697102138535&format=application/json&lang=es&startTime=2014-02-07&endTime=2014-03-07&API_KEY=tntmnZ85PrQ6AGw6ihuUGhqs894jl66UI8p4ph4rmbvcno3MIApKE0o2N1awzT2p')
-open = urllib2.build_opener()
-f = opener.open(request)
-json = json.loads(f.read())
-print json
+import sys
+
+web = raw_input("web es http://")
+userAgent = "NuestroNavegador"
+headers = { 'User-Agent' : userAgent }
+req = urllib2.Request("http://"+web , None, headers)
+response = urllib2.urlopen(req)
+print response.read()
+response.close()
 
